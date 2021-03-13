b44a64c1a4cc402448f35bee5f8108e8902ed83f
André Mion <andremion@gmail.com>
2021-03-13 19:16:45 +0000

    Run Spotless

--- app/src/main/java/com/example/androiddevchallenge/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/login/LoginScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.login
 
 import androidx.compose.foundation.Image
@@ -31,7 +46,7 @@ import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.component.InputText
 import com.example.androiddevchallenge.ui.component.PrimaryButton
 import com.example.androiddevchallenge.ui.theme.MyTheme
-import java.util.*
+import java.util.Locale
 
 @Composable
 fun LoginScreen(onLoginClick: () -> Unit) {
